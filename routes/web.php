bfdd4e11bab3f66cf1c1007177ffd38f94127440
Dias Utsman <utsmand91@gmail.com>
2023-06-06 07:47:10 +0700

    Add list of routes in root

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $routes = Route::getRoutes();
+    $collection = [];
+    foreach ($routes as $value) {
+        if (Str::startsWith($value->uri, 'api')) {
+            $collection[] = collect($value)->only(['uri', 'methods'])
+                ->map(fn ($item, $key) => $key == 'methods' ? $item[0] : $item);
+        }
+    }
+
+    return $collection;
 });
